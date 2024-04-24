4184488a8c30c9901a7cc5d95b86c520bbece56c
Захар Самойлов <tormaz557@gmail.com>
2024-04-24 19:54:56 +0300

    Создание основных окон.

--- TeatrVseDela/Form1.Designer.cs
+++ TeatrVseDela/Form1.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace TeatrVseDela
+﻿using System;
+using System.Windows.Forms;
+
+namespace TeatrVseDela
 {
     partial class Form1
     {
@@ -22,6 +25,8 @@
 
         #region Код, автоматически созданный конструктором форм Windows
 
+        
+
         /// <summary>
         /// Требуемый метод для поддержки конструктора — не изменяйте 
         /// содержимое этого метода с помощью редактора кода.
@@ -29,12 +34,120 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.toolTip3 = new System.Windows.Forms.ToolTip(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.toolTip4 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGray;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(18, 16);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 75);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Театры";
+            this.toolTip1.SetToolTip(this.button1, "Открывает окно с театрами Коми");
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button_Theater);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightGray;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(199, 16);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 75);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Представления";
+            this.toolTip2.SetToolTip(this.button2, "Открывает окно с репертуаром всех театров Коми");
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button_Repertuar);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup_1);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightGray;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(18, 511);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(235, 75);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Выйти из программы";
+            this.toolTip3.SetToolTip(this.button3, "Закрывает программу");
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button_Exit);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LightGray;
+            this.button4.Font = new System.Drawing.Font("Monotype Corsiva", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(789, 527);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(36, 60);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "?";
+            this.toolTip4.SetToolTip(this.button4, "Открывает инструкци по пользованию программой");
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button_instruction);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(837, 599);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Monotype Corsiva", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Театры Коми";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private ToolTip toolTip1;
+        private ToolTip toolTip2;
+        private Button button3;
+        private ToolTip toolTip3;
+        private Button button4;
+        private ToolTip toolTip4;
     }
 }
 
--- TeatrVseDela/Form1.cs
+++ TeatrVseDela/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace TeatrVseDela
 {
@@ -15,6 +16,39 @@ namespace TeatrVseDela
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button_Theater(object sender, EventArgs e)
+        {
+            TheaterWindow theaterWindow = new TheaterWindow();
+            theaterWindow.Show();
+        }
+
+        private void button_Repertuar(object sender, EventArgs e)
+        {
+            ReperuarWindows reperuarWindows = new ReperuarWindows();
+            reperuarWindows.Show();
+        }
+
+        private void toolTip1_Popup_1(object sender, PopupEventArgs e)
+        {
+            
+        }
+
+        private void button_Exit(object sender, EventArgs e)
+        {
+          Application.Exit();
+        }
+
+        private void button_instruction(object sender, EventArgs e)
+        {
+            MessageBox.Show("Данная программа познакомит вас с Театрами Коми и их репертуаром." +
+                "Наведеите курсор на кнопку, чтобы узнать, что она делает.", "Инструкция", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
